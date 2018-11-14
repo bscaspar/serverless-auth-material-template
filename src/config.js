a4ad964e1c7e1dc56597d2c3e99ef745355513bc
bscaspar <bradyscaspar@outlook.com>
2018-11-13 20:05:00 -0800

    updating config file to remove values

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
-    REGION: "us-west-2",
-    BUCKET: "serverless-tutorial-bc"
+    REGION: "",
+    BUCKET: ""
   },
   apiGateway: {
-    REGION: "us-west-2",
-    URL: "https://krbln2jeoi.execute-api.us-west-2.amazonaws.com/prod"
+    REGION: "",
+    URL: ""
   },
   cognito: {
-    REGION: "us-west-2",
-    USER_POOL_ID: "us-west-2_PKCeItuxz",
-    APP_CLIENT_ID: "1q2asupl82uqm2475l9ph05vpp",
-    IDENTITY_POOL_ID: "us-west-2:9a9f38b5-5054-451d-91cc-bd8b744a867a"
+    REGION: "",
+    USER_POOL_ID: "",
+    APP_CLIENT_ID: "",
+    IDENTITY_POOL_ID: ""
   }
-}
+};
